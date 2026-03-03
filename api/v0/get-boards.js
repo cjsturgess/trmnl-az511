4441ea4e931070327d89a3d5ef3095486adcd5b7
CJ Sturgess <cj@sturgess.co>
2026-03-03 14:46:23 -0700

    Add endpoint for message board search

---
+++ api/v0/get-boards.js
@@ -0,0 +1,56 @@
+/**
+ * get-boards.js
+ * Given an AZ511 API key, will return a list of all boards for use with the TRMNL configuration dropdown.
+ */
+
+module.exports = async (req, res) => {
+    const api_key = req.query.api_key,
+        query = req.body.query;
+
+    /**
+     * API Key Validation
+     */
+    if (!api_key)
+        return res.json({
+            "error": "Must provide an AZ511 API key."
+        });
+
+    /**
+     * Fetch AZ511 Message Boards
+     */
+    const api_response = await fetch(`https://az511.com/api/v2/get/messagesigns?key=${api_key}&format=json`);
+    
+    if (api_response.status != 200) {
+        const api_error = await api_response.text();
+        if (api_error.includes("Invalid Key"))
+            return res.json({
+                "error": "Failed to query the AZ511 API due to an invalid key."
+            });
+        else
+            return res.json({
+                "error": "An unknown error occurred whilst querying the AZ511 API."
+            });
+    }
+
+    let message_boards;
+    try {
+        message_boards = await api_response.json();
+    } catch (e) {
+        return res.json({
+            "error": "Failed to parse the AZ511 API query response."
+        });
+    }
+
+
+    let available_boards = message_boards;
+    if (query) available_boards = available_boards.filter(x => x.Name.toLowerCase().includes(query.toLowerCase()));
+
+    const board_map = available_boards.map(x => {
+        return {
+            "id": x.Id,
+            "name": x.Name
+        };
+    });
+
+    res.json(board_map);
+};
